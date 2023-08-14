d5677cb69d28b19c184b9ad65c332eafe11d0c78
canasmh <canasmh@yahoo.com>
2023-08-14 17:37:06 -0400

    Do not show footer if user is on welcome page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'iron-track';
+  showFooter: boolean = true;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd))
+      .subscribe(() => {
+        this.showFooter = this.activatedRoute.firstChild?.snapshot.data['showFooter'] !== false;
+      });
+  }
 }
