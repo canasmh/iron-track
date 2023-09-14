87003c5c0f8318f80bccfc6b9214458bff382754
canasmh <canasmh@yahoo.com>
2023-09-13 23:02:28 -0400

    Set up signup post request

---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, catchError } from 'rxjs';
+import { User } from '../types/customTypes';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private apiUrl = 'http://localhost:8080';
+
+  constructor(private http: HttpClient) { }
+
+  signup(user: User): Observable<any> {
+    return this.http.post(`${this.apiUrl}/signup`, user)
+  }
+}
+
+
