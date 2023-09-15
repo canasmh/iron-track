f3de203115efadee2c5d2eb6a161d131e4a82ff8
canasmh <canasmh@yahoo.com>
2023-09-15 13:36:30 -0400

    Remove hardcoded routine

--- src/app/routine/routine.component.ts
+++ src/app/routine/routine.component.ts
@@ -10,66 +10,13 @@ import { Routine } from '../shared/types/customTypes';
 })
 export class RoutineComponent implements OnInit {
 
-  routine: Routine = {
-    name: 'Chest & Biceps',
-    exercises: [
-      {
-        workout: 'Bench Press',
-        weight: "175",
-        weightUnit: "lbs",
-        sets: "3",
-        quantity: "10",
-        unit: "reps",
-      },
-      {
-        workout: 'Bicep Curl',
-        weight: "25",
-        weightUnit: "lbs",
-        sets: "3",
-        quantity: "10",
-        unit: "reps",
-      },
-      {
-        workout: 'Push up',
-        weight: "0",
-        weightUnit: "lbs",
-        sets: "3",
-        quantity: "30",
-        unit: "sec",
-      },
-      {
-        workout: 'Hammer Curl',
-        weight: "25",
-        weightUnit: "lbs",
-        sets: "3",
-        quantity: "30",
-        unit: "sec",
-      },
-      {
-        workout: 'Pec Flys',
-        weight: "125",
-        weightUnit: "lbs",
-        sets: "3",
-        quantity: "10",
-        unit: "reps",
-      },
-      {
-        workout: 'Preacher Curl',
-        weight: "50",
-        weightUnit: "lbs",
-        sets: "3",
-        quantity: "30",
-        unit: "sec",
-      },
-    ]
-  };
+  routine?: Routine;
 
   constructor(private route: ActivatedRoute, private routinesService: RoutinesService) {}
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       const all_routines = this.routinesService.getRoutines();
-      all_routines.push(this.routine);
       this.routine = all_routines.filter(routine => routine.name === params['routine_name'])[0];
     })
   }
