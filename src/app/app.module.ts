e017da96530d997101faed9c0a46e2d89693d2cc
canasmh <canasmh@yahoo.com>
2023-10-25 17:36:27 -0400

    Import a11y module
    This module is used for focus traping, which I need in the safe-delete modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,8 @@ import { AddRoutineComponent } from './add-routine/add-routine.component';
 import { AddRoutineFinalComponent } from './add-routine-final/add-routine-final.component';
 import { HttpClientModule } from '@angular/common/http';
 import { RoutineComponent } from './routine/routine.component';
+import { EditRoutineComponent } from './edit-routine/edit-routine.component';
+import { A11yModule } from '@angular/cdk/a11y';
 
 @NgModule({
   declarations: [
@@ -26,13 +28,15 @@ import { RoutineComponent } from './routine/routine.component';
     HomeComponent,
     AddRoutineComponent,
     AddRoutineFinalComponent,
-    RoutineComponent
+    RoutineComponent,
+    EditRoutineComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    A11yModule
   ],
   providers: [],
   bootstrap: [AppComponent]
