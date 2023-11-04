5a86e2faefde61bd17a912e664eb8c4d0f01f27f
canasmh <canasmh@yahoo.com>
2023-11-04 05:17:55 -0400

    Remove beforeunload event listener
    Not really working how I wanted it to, so..

--- src/app/workout/workout/workout.component.ts
+++ src/app/workout/workout/workout.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostListener, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { WorkoutService } from 'src/shared/services/workout.service';
 import { Workout } from 'src/shared/types/Workout';
@@ -12,28 +12,10 @@ export class WorkoutComponent implements OnInit {
 
   private workoutId: string;
   workout: Workout;
-  isPageReload: boolean = false;
 
   finishWorkout() {
     this.workoutService.finishWorkout();
     this.router.navigate(['/routines', this.workout.routine?.id]);
-  }
-
-  @HostListener('window:beforeunload', ['$event'])
-  confirmNavigation($event: any) {
-
-    if (this.isPageReload) {
-
-      return;
-    }
-
-    $event.preventDefault();
-
-    if (!this.workoutService.workoutFinished) {
-      $event.returnValue = this.workoutService.finishWorkout();
-    }
-
-    return $event;
   }
 
   constructor(
@@ -41,14 +23,6 @@ export class WorkoutComponent implements OnInit {
     private router: Router,
     private workoutService: WorkoutService
   ) {
-    const type = performance.getEntriesByType('navigation')[0].toJSON().type;
-
-    if (type === 'reload') {
-      this.isPageReload = true;
-    } else {
-      this.isPageReload = false;
-    }
-
     const workoutJson = localStorage.getItem('workout');
     const workout: Workout = workoutJson ? JSON.parse(workoutJson) : null;
     this.workout = this.workoutService.getWorkout();
