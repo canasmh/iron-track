3b88e4fd8d589fdc58b594d31400d5268f0a6d3b
canasmh <canasmh@yahoo.com>
2023-11-10 17:21:08 -0500

    Remove hard coded values for the workout sets

--- src/app/workout/set/set.component.ts
+++ src/app/workout/set/set.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { WorkoutService } from 'src/shared/services/workout.service';
-import { Workout } from 'src/shared/types/Workout';
+import { RoutineExercise } from 'src/shared/types/RoutineExercise';
+import { Workout, WorkoutSet } from 'src/shared/types/Workout';
 
 @Component({
   selector: 'app-set',
@@ -10,8 +12,21 @@ import { Workout } from 'src/shared/types/Workout';
 export class SetComponent {
 
   workout: Workout;
-  constructor(private workoutService: WorkoutService) {
+  sets?: WorkoutSet[];
+  weight?: string;
+  quantity?: string;
+  name?: string;
+
+  constructor(private route: ActivatedRoute, private workoutService: WorkoutService) {
+    const routineExerciseId = route.snapshot.params['routine_exercise_id'];
     this.workout = workoutService.getWorkout();
+
+    const routineExercise: RoutineExercise | undefined = this.workout.routine?.exercises.find(exercise => exercise.id == routineExerciseId);
+
+    this.sets = new Array(routineExercise?.sets);
+    this.weight = routineExercise?.weight;
+    this.quantity = `${routineExercise?.quantity} ${routineExercise?.quantityUnit.slice(0, 1).toUpperCase()}${routineExercise?.quantityUnit.slice(1)}(s)`;
+    this.name = routineExercise?.exercise.name;
   }
 
 }
