a7ef53b46b5194f964cfd098bbd05fdc65852e9a
canasmh <canasmh@yahoo.com>
2023-12-07 11:57:16 -0500

    Remove redundant navigate method

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,24 +1,11 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { UserService } from '../../shared/services/user.service';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
+
 export class FooterComponent {
-
-  constructor(private route: ActivatedRoute, private userService: UserService, private router: Router) {
-  }
-  routineId: string = this.route.snapshot.params['routine_id'];
-
-  navigateToProfile() {
-    this.router.navigate(['/profile']);
-
-  }
-
-  ngOnInit(): void {
-  }
 }
 
