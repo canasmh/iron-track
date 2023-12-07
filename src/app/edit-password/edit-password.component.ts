09eeca7f1ddce7cb436d05ad75cfa9bb7104e447
canasmh <canasmh@yahoo.com>
2023-12-07 11:37:04 -0500

    Fix linter issues

--- src/app/edit-password/edit-password.component.ts
+++ src/app/edit-password/edit-password.component.ts
@@ -1,23 +1,23 @@
-import {Component, OnInit} from '@angular/core';
-import {OverlayService} from "../../shared/services/overlay.service";
-import {AbstractControl, FormControl, FormGroup, Validators} from "@angular/forms";
-import {User} from "../../shared/types/User";
-import {UserService} from "../../shared/services/user.service";
-import {ErrorMessageService} from "../../shared/services/error-message.service";
+import { Component, OnInit } from '@angular/core';
+import { OverlayService } from '../../shared/services/overlay.service';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { User } from '../../shared/types/User';
+import { UserService } from '../../shared/services/user.service';
+import { ErrorMessageService } from '../../shared/services/error-message.service';
 
 @Component({
   selector: 'app-edit-password',
   templateUrl: './edit-password.component.html',
   styleUrls: ['./edit-password.component.scss']
 })
-export class EditPasswordComponent implements OnInit{
+export class EditPasswordComponent implements OnInit {
   submitted: boolean = false;
   editPasswordForm!: FormGroup;
   errorMessage?: string | null;
-  userObject: User ={
-    email: "", password: "",
-    name: ''};
-
+  userObject: User = {
+    email: '', password: '',
+    name: ''
+  };
 
   constructor(private overlayServicePass: OverlayService, private userService: UserService, private errorMessageService:ErrorMessageService) {
   }
@@ -25,28 +25,30 @@ export class EditPasswordComponent implements OnInit{
   ngOnInit(): void {
     this.editPasswordForm = new FormGroup({
       password: new FormControl('', [
-      Validators.required,
-      Validators.minLength(5),
-      Validators.maxLength(72)
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(72)
       ])
-    })
+    });
 
     this.editPasswordForm.valueChanges.subscribe(() => {
       this.submitted = false;
-    })
+    });
   }
 
   onSubmit() {
     this.submitted = true;
-    this.errorMessage = null
-    console.log(this.editPasswordForm.value)
-    console.log("invalid", this.editPasswordForm.invalid)
+    this.errorMessage = null;
+    console.log(this.editPasswordForm.value);
+    console.log('invalid', this.editPasswordForm.invalid);
+
     if (!this.editPasswordForm.invalid ) {
       this.userObject.password = this.editPasswordForm.value.password.trim();
-      this.userService.editPassword({password: this.userObject.password}, this.oldPassword).subscribe({
+      this.userService.editPassword({ password: this.userObject.password }, this.oldPassword).subscribe({
         next:(data) => {
+          console.log(data);
         }
-      })
+      });
     } else {
       if (this.password.invalid) {
         this.getErrorMessage(this.password, 'Password');
@@ -57,10 +59,10 @@ export class EditPasswordComponent implements OnInit{
     }
   }
 
-  get password(){
+  get password() {
     return this.editPasswordForm.controls['password'].value;
   }
-  get oldPassword(){
+  get oldPassword() {
     return this.editPasswordForm.controls['oldPassword'].value;
   }
   closeOverlay() {
